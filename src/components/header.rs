7057235572a3a674a1bbdd7a11e5ffbf69a61281
kemoshumai <87066961+kemoshumai@users.noreply.github.com>
2024-11-17 01:02:52 +0900

    ヘッダーを上部に固定

--- src/components/header.rs
+++ src/components/header.rs
@@ -4,7 +4,7 @@ use leptos::{component, view, IntoView};
 pub fn HeaderComponent() -> impl IntoView{
 
     view! {
-        <div class="w-full flex justify-center bg-sky-400 p-2" >
+        <div class="w-full flex justify-center bg-sky-400 p-2 sticky top-0" >
             <div class="bg-transparent">
                 <a href="/">
                     <img src="./public/logo.png" class="bg-transparent h-12 w-auto" />
