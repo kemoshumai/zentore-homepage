8dc7b6ee1fb2c21e8b622ca5474954e9d043b3af
kemoshumai <87066961+kemoshumai@users.noreply.github.com>
2024-11-17 00:01:02 +0900

    スタイルを初期化

--- src/lib.rs
+++ src/lib.rs
@@ -17,10 +17,10 @@ pub fn App() -> impl IntoView {
     provide_meta_context();
 
     view! {
-        <Html lang="en" dir="ltr" attr:data-theme="light"/>
+        <Html lang="ja" dir="ltr" attr:data-theme="light"/>
 
         // sets the document title
-        <Title text="Welcome to Leptos CSR"/>
+        <Title text="全トレ -全部トレーニング- 公式ホームページ"/>
 
         // injects metadata in the <head> of the page
         <Meta charset="UTF-8"/>
--- src/pages/not_found.rs
+++ src/pages/not_found.rs
@@ -3,5 +3,11 @@ use leptos::*;
 /// 404 Not Found Page
 #[component]
 pub fn NotFound() -> impl IntoView {
-    view! { <h1>"Uh oh!" <br/> "We couldn't find that page!"</h1>  }
+    view! { 
+        <div>
+            <h1>"404 Not Found"</h1>
+            <p>"浪人生にはページを見つけることができなかった。"</p>
+            <a href="/">"トップページへ"</a>
+        </div>
+    }
 }
