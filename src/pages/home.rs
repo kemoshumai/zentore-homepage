513f451290b23ea3193c17b84097e64d3a5798fd
kemoshumai <87066961+kemoshumai@users.noreply.github.com>
2024-11-17 01:21:35 +0900

    ホームページにゲーム説明用のセクションを空で用意

--- src/pages/home.rs
+++ src/pages/home.rs
@@ -8,6 +8,10 @@ pub fn Home() -> impl IntoView {
     view! {
         <div class="bg-sky-700">
             <Jumbotron />
+            <div class="bg-sky-500 h-96">
+                <h2 class="bg-transparent text-5xl text-white font-bold" style="transform: rotate(-12deg) translateY(-48px); -webkit-text-stroke: 1px black; text-stroke: 1px black; ">"運動しながら"<br />"問題も解こう！！"</h2>
+                <p class="bg-transparent">""</p>
+            </div>
         </div>
     }
 }
