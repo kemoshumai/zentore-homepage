4da46c78f58aa6d22a5cbabb093e2d8d9cef1c16
kemoshumai <87066961+kemoshumai@users.noreply.github.com>
2024-11-17 00:43:47 +0900

    ヘッダーを作成

--- src/components/header.rs
+++ src/components/header.rs
@@ -4,7 +4,13 @@ use leptos::{component, view, IntoView};
 pub fn HeaderComponent() -> impl IntoView{
 
     view! {
-        <p>"全トレヘッダー"</p>
+        <div class="w-full flex justify-center bg-sky-400 p-2" >
+            <div class="bg-transparent">
+                <a href="/">
+                    <img src="./public/logo.png" class="bg-transparent h-12 w-auto" />
+                </a>
+            </div>
+        </div>
     }
 
 }
